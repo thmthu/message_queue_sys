17e4a6ba2d87b68076483aebefe22593839a177a
thmthu <thmt.thu@gmail.com>
2025-04-02 01:02:36 +0700

    add queue handle Dead letter

--- src/services/consumerService.js
+++ src/services/consumerService.js
@@ -11,5 +11,54 @@ const messageService = {
       console.log("cusumer service: ", err);
     }
   },
+  consumerToQueueNormal: async () => {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+      const notifQueue = "notiQueue";
+      channel.consume(notifQueue, (msg) => {
+        const noti = msg.content.toString();
+        console.log("SEND notificationQueue successfully processed:", noti);
+        channel.ack(msg);
+        
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  consumeToQueueFailed: async () => {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+      const notiDlxExchange = "notiDlxExchange";
+      const notiRoutingKeyDlx = "notiRoutingKeyDlx";
+      const notQueueHandler = "notificationQueueHotFix";
+
+      await channel.assertExchange(notiDlxExchange, "direct", {
+        durable: true,
+      });
+
+      const queueResult = await channel.assertQueue(notQueueHandler, {
+        exclusive: false,
+      });
+
+      await channel.bindQueue(
+        queueResult.queue,
+        notiDlxExchange,
+        notiRoutingKeyDlx
+      );
+      await channel.consume(
+        queueResult.queue,
+        (msgFailed) => {
+          console.log(
+            "has notification error:, pls hack fix:",
+            msgFailed.content.toString()
+          );
+        },
+        { noAck: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
+
 module.exports = { messageService };
